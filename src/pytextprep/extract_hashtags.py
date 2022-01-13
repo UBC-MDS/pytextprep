0c4096fa4e36af0ffd0987b59fe2c264ce1699f0
Melisa Maidana <88546099+mmaidana24318@users.noreply.github.com>
2022-01-12 23:37:08 -0800

    feat: Create extract_hashtags.py

---
+++ src/pytextprep/extract_hashtags.py
@@ -0,0 +1,15 @@
+def extract_hashtags(tweets):
+    """Extracts all hashtags included in the input tweet data file
+    
+    Parameters
+    ----------
+    tweets : str
+        Path to a text file containing tweets.
+    Returns
+    -------
+    list
+        List of hashtags included in the tweets file
+    Examples
+    --------
+    >>> extract_hashtags("tweets.txt")
+    """
